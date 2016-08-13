7e1eb9d1b273938bea56bee8c9ada4e39a46af76
Mayowa Adegbola <mayowa.adegbola@andela.com>
2016-08-13 15:18:57 +0100

    Set indentation on artist class

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,42 @@
 class Artist
-	extend Concerns::Findable
-	attr_accessor :name
-	attr_reader :songs
+  extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :songs
 
-	@@all = []
+  @@all = []
 
-	def initialize(name)
-		@name = name
-		@songs =[]
-	end
+  def initialize(name)
+    @name = name
+    @songs =[]
+  end
 
-	def self.all
-		@@all
-	end
+  def self.all
+    @@all
+  end
 
-	def self.destroy_all
-		@@all = []
-	end
+  def self.destroy_all
+    @@all = []
+  end
 
-	def self.create(name)
-		new(name).save
-	end
+  def self.create(name)
+    new(name).save
+  end
 
-	def save
-		@@all << self
-		self
-	end
+  def save
+    @@all << self
+    self
+  end
 
-	def add_song(song)
-		@songs.push(song) unless @songs.include?(song)
-		song.artist = self unless song.artist == self
-	end
+  def add_song(song)
+    @songs.push(song) unless @songs.include?(song)
+    song.artist = self unless song.artist == self
+  end
 
-	def genres
-		self.songs.collect{|s| s.genre}.uniq
-	end
+  def genres
+    self.songs.collect{|s| s.genre}.uniq
+  end
 
-	def to_s
+  def to_s
     "#{name}"
   end
 end
